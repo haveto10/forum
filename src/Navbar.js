02d65155ddf201c2e1118f481b85f37e610b91da
Ivan Dapena <ivan.dapena@solera.com>
2022-10-06 12:12:48 +0200

    Menu Hamburguer Created

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,22 +1,31 @@
+import { color } from "@mui/system";
 import React from "react";
 import { Link } from "react-router-dom";
+import './Navbar.css'
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/register">register</Link>
-        </li>
-        <li>
-          <Link to="/newpost">new post</Link>
-        </li>
+    <div className="respmenu">
+      <input type="checkbox" />
+      <i class="fas fa-bars"></i>
+      <i class="fas fa-times"></i>
+      <nav>
+        <ul>
 
-      </ul>
-    </nav>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <Link to="/newpost">New Post</Link>
+          </li>
+
+        </ul>
+      </nav>
+    </div>
+
   );
 }
 
