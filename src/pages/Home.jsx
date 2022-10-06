cb12656b373a1f76a2d7794986df5c4e44b9ba47
Ivan Dapena <ivan.dapena@solera.com>
2022-10-06 15:42:58 +0200

    Home Frontend V1 - Incluye Favicon

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import '../home.css';
 
 function Home() {
     return (
-        <h1>this is the homepage</h1>
+        <div className='body'>
+            <div className='contenedor'>
+                <h2>Login</h2>
+                <form id="forma" name="forma" method="POST">
+                    <div class="elemento">
+                        <label for="usuario">User</label>
+                        <input type="text" id="usuario" name="usuario" required="true"/>
+                    </div>
+                    <div class="elemento">
+                        <label for="password">Password</label>
+                        <input type="password" id="password" name="password" required="true"/>
+                    </div>
+                    <div class="elemento">
+                        <input type="submit" value="Send"/>
+                    </div>
+                </form>
+            </div>
+        </div>
+
     );
 }
 
